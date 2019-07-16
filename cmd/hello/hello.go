1426991ac3b019ea04c8839159a10d9aa1793e27
yusufkaratoprak <yusufkaratoprak@yahoo.com>
2019-07-16 20:02:02 +0300

    adding yusuf

--- cmd/hello/hello.go
+++ cmd/hello/hello.go
@@ -11,7 +11,7 @@ import (
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello world !\n")
+		fmt.Fprintf(w, "Hello world . My name is YUSUF!\n")
 	})
 
 	http.ListenAndServe(":8090", nil)
